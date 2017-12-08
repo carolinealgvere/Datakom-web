c81bf539f7fd89a3ebbd97272b540b4bd36d7860
carolinealgvere <caroline.algvere@hotmail.com>
2017-12-08 09:31:32 +0100

    Add files via upload

--- public/js/plane.js
+++ public/js/plane.js
@@ -20,22 +20,40 @@ function DisplayItems(){
     var tr = document.createElement('TR');
     table.appendChild(tr);
     var i = 0;
-	for(i; i < 10; i++){
+	for(i; i < 30; i++){
 		var th = document.createElement('TH');
 		tr.appendChild(th);
-		var txt = document.createTextNode("hej");
+		var txt = document.createTextNode(i+1);
         th.appendChild(txt); 
     }
-    var tr1 = document.createElement('TR');
-    table.appendChild(tr1);
-    var n = 0;
-	for(n; n < 10; n++){
-		var td = document.createElement('TD');
-		tr1.appendChild(td);
-		var txt = document.createTextNode("hej");
-        td.appendChild(txt); 
-    }
+    var k = 0;
+    for(k; k<7; k++){
+        if(k == 3){
+            var tr1 = document.createElement('TR');
+            table.appendChild(tr1);
+            var m=0;
+            for(m; m<30; m++){
+                var td1 = document.createElement('TD');
+                tr1.appendChild(td1);
+                var txt = document.createTextNode("-");
+                td1.appendChild(txt);
+            }
+        } else{
+            var tr1 = document.createElement('TR');
+            table.appendChild(tr1);
+            var n = 0;
+	        for(n; n < 30; n++){
+		        var td = document.createElement('TD');
+		        tr1.appendChild(td);
+                var button = document.createElement("button");
+                var id = "seat_" + k.toString() + n.toString();
+                button.setAttribute("name", id);
+                //button.style.background='#007800';                
+                td.appendChild(button);
+                console.log(button.name);
+            }
+        }   
     document.body.appendChild(table);
         
-
+    }
 }
