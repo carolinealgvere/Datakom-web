72e40927e63ded7781dad3f77179139d67b2c31d
MrMunter <victormunthe@gmail.com>
2017-12-08 09:36:44 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
+var path = require('path');
+var router =express.Router();
+app.use(router);
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+
+var server = app.listen(8080);
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/seat',function(req,res){
+//	console.log(req);
+	res.send("HELLO");
+})
+app.post('/login',function(req,res){
+	console.log(req.body);
+	var bookingNumber = req.body.bnr;
+	var name = req.body.tholder;
+	if (name == "hej" && bookingNumber == "123"){
+		res.render(__dirname +'/views/sida2', { 
+			name: "Name: " + name, 
+			bookingNumber: "Booking number: " + bookingNumber});
+		//res.sendFile(__dirname + '/views/sida2.ejs');	
+		//res.send(bookingNumber);
+	}
+	else{
+		res.sendFile(__dirname +'/views/index.html');
+	}
+})
+
+
+
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
