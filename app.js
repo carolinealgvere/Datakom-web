4d9e9889e608c3aa5deef30595d0a04e7b862500
MrMunter <victormunthe@gmail.com>
2017-12-11 13:18:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var bodyParser = require('body-parser');
 var express = require('express');
-
+var mysql = require('mysql');
 var app = express();
 var path = require('path');
 var router =express.Router();
+var passport = require('passport'), 
+LocalStrategy = require('passport-local').Strategy;
 app.use(router);
-
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 //app.set('views', __dirname + '/views');
@@ -14,48 +15,100 @@ var server = app.listen(8080);
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+//brew services start mysql
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+});
+
+
+
+
+ // app.use(express.cookieParser());
+  //app.use(express.bodyParser());
+  //app.use(express.cookieSession()); 
+  app.use(passport.initialize());  
+  app.use(passport.session());
+
+
 app.get('/seat',function(req,res){
 //	console.log(req);
 	res.send("HELLO");
 })
+
 app.post('/login',function(req,res){
-	console.log(req.body);
+
+
 	var bookingNumber = req.body.bnr;
 	var name = req.body.tholder;
 	var seat = "You have not selected a seat yet";
-	if (name == "hej" && bookingNumber == "123"){
-		res.render(__dirname +'/views/sida2', { 
-			name: "Name: " + name, 
-			bookingNumber: "Booking number: " + bookingNumber,
-			seat: seat});
-	}
-	else if(name == "hello" && bookingNumber =="456")
-		res.render(__dirname +'/views/sida2', { 
-			name: "Name: " + name, 
-			bookingNumber: "Booking number: " + bookingNumber,
-			seat: seat});
-	else{
+	var sql = 'SELECT * FROM sys.Users WHERE fullName = ? AND bookingNumber = ?';
+	//req.session.username = name;
+	//console.log(req.session);
+	con.query(sql, [name, bookingNumber], function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    if(!result.length){
 		res.sendFile(__dirname +'/views/index.html');
-	}
+    }
+    else{
+		res.render(__dirname +'/views/sida2', { 
+			name: "Name: " + name, 
+			bookingNumber: "Booking number: " + bookingNumber,
+			seat: seat});
+			
+    }	
+  });
 })
 
+
+
 app.post('/seatSelected', function(req,res){
-	//var seat = req.body.;
+	
 	var seat = req.body.seat;
 	console.log(req.body.seat);
+	var sql1 = 'SELECT * FROM sys.Seats WHERE seat =? AND Taken =?';
+	con.query(sql1, [seat, 0],function (err,result,fields){
+		if (err) throw err;
+
+		console.log(result);
+	/* Ändringar som behövs
+	1. Vi måste lägga till så att den förra stolen personen satt på ändras tillbaka till Taken=0 och bookingnumber = null
+	2. Vi måste lägga till så att det är den inloggade personens bokningsnummer som läggs in och inte "1000"
+	*/
+
+
+
+
+	if(result.length){
+		var sql2 = "UPDATE sys.Seats SET bookingNumber = ? WHERE seat = ?";
+		con.query(sql2, [1000,seat] , function(err, result){
+			if (err) throw err;
+		});
+
+	var sql3 = "UPDATE sys.Seats SET Taken = ? WHERE seat = ?";
+	con.query(sql3, [1,seat] , function(err, result){
+		if (err) throw err;
+	});
 	res.render(__dirname +'/views/sida2', {
 		name:"funkar inte",
 		bookingNumber:"funkar inte",
 		seat:"Your seat is: "+ seat
 	});
 
+	}
+	else{
+		console.log("Seat alredy taken");
 
-
-
+		res.sendFile(__dirname +'/views/flygplan.html');
+	}
+	});
 })
-
-
-
 
 
 app.use(express.static(__dirname + '/public'));
