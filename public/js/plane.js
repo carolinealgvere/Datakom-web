9871384aede6ce8f96c0d5cb0d41edb740ab3a58
MrMunter <victormunthe@gmail.com>
2017-12-12 11:49:21 +0100

    Add files via upload

--- public/js/plane.js
+++ public/js/plane.js
@@ -1,4 +1,5 @@
 
+
 function docLoaded(fn){
     if(document.readyState !== 'loading'){
         fn();
@@ -45,47 +46,33 @@ function DisplayItems(){
                 var button = document.createElement("button");
                 button.setAttribute("name", "seat");
                 var text = k.toString() + n.toString();
-                var OBS = "OBS! You have selected a seat which requires you to open emergency doors in case of energancy. Hence if you are traveling with child, have a disability etc. please select a new seat.";
+                //var OBS = "OBS! You have selected a seat which requires you to open emergency doors in case of energancy. Hence if you are traveling with child, have a disability etc. please select a new seat.";
                 if(text.charAt(0) == 0){
-                    //if(text.charAt(1)==1 && (text.charAt(2)==3 || text.charAt(2)==4 || text.charAt(2)==5)){
-                      //  text = "A" + n.toString() +  "\n" + OBS;
-                       // console.log("du klickade rätt iaf"); 
-                   // }else{
                         text = "A" + n.toString() ; 
                     }                   
                 else if(text.charAt(0) == 1){
-                    /*if(text.charAt(1)==1 && (text.charAt(2)==3 || text.charAt(2)==4 || text.charAt(2)==5)){
-                        text = "B"  + n.toString()  + OBS; 
-                    }else{*/
                         text = "B" + n.toString() ; 
                     }                   
                 else if(text.charAt(0) == 2){
-                    /*if(text.charAt(1)==1 && (text.charAt(2)==3 || text.charAt(2)==4 || text.charAt(2)==5)){
-                        text = "C" + n.toString() +  OBS; 
-                    }else{*/
                         text = "C" + n.toString()  ; 
                     }
                 else if(text.charAt(0) == 4){
-                    /*if(text.charAt(1)==1 && (text.charAt(2)==3 || text.charAt(2)==4 || text.charAt(2)==5)){
-                        text = "D" + n.toString() + OBS; 
-                    }else{*/
+
                         text = "D" +n.toString() ; 
                     }        
                 else if(text.charAt(0) == 5){
-                    /*if(text.charAt(1)==1 && (text.charAt(2)==3 || text.charAt(2)==4 || text.charAt(2)==5)){
-                        text ="E" + n.toString() +  OBS; 
-                    }else{*/
+
                         text = "E" + n.toString() ; 
                     }        
                 else if(text.charAt(0) == 6){
-                   /* if(text.charAt(1)==1 && (text.charAt(2)==3 || text.charAt(2)==4 || text.charAt(2)==5)){
-                        text = "F" + n.toString() +  OBS; 
-                    }else{*/
+
                         text = "F" + n.toString(); 
                     }        
                 else{
                    text = k.toString() + n.toString();
             }
+
+
 
                 button.setAttribute("value", text);
                 button.setAttribute("type", "submit");        
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,10 @@
-var socket = io.connect('http://130.238.244.141:3000');
+var socket = io.connect('http://130.238.242.231:8080');
 socket.on('connect', function(data) {
    socket.emit('join', 'Hello World from client');
 });
+
+/*var clickButton{
+
+	var btn = document.createButton("loginButton");
+	btm.setAttribute('onClick');
+}*/
